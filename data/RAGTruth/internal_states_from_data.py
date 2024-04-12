bf328925c64b08cec0190eb57712f7d1bafe13fc
F4biian <fabian@fotofactory-ridder.de>
2024-04-12 13:38:55 +0200

    Adjusted script to new architecture changes

--- data/RAGTruth/internal_states_from_data.py
+++ data/RAGTruth/internal_states_from_data.py
@@ -1,16 +1,28 @@
 # This should be called in the HalluRAG directory due to an import error that would occur otherwise.
 # e.g.: python3 data/RAGTruth/internal_states_from_data.py
 
-# TODO: Remove when publishing
-import os
-os.environ['HF_HOME'] = '/data2/quokka/.cache'
 
+########################################################################################
+# IMPORTS
+
+import os
+import warnings
+from dotenv import load_dotenv
+
+# Loading env variables (happens before importing models for the case if HF_HOME is changed)
+load_dotenv()
+
+# Importing models from own architecture
 from models import Mistral_7B_Instruct_V1, LLaMA2_7B_ChatHF
 
+########################################################################################
 
-llm = LLaMA2_7B_ChatHF(auto_load=True)
 
-print(llm.generate("What is the capital of Austria, but spelled backwards?"))
-# print(len(llm.get_internal_states("What is the capital of Austria, but spelled backwards?", 'The capital of Austria, spelled backwards, is "Austria".')))
+if __name__ == "__main__":
+    # Ignore warnings
+    warnings.filterwarnings("ignore")
 
-llm.unload()
+    # with Mistral_7B_Instruct_V1() as llm:
+    with LLaMA2_7B_ChatHF(os.environ['HF_TOKEN']) as llm:
+        print(llm.generate("What is the capital of Austria, but spelled backwards?"))
+        # print(len(llm.get_internal_states("What is the capital of Austria, but spelled backwards?", 'The capital of Austria, spelled backwards, is "Austria".')))
