65eaf3f6a3c2d986cade334bd284ec9b013fc33c
F4biian <fabian@fotofactory-ridder.de>
2024-04-12 13:31:46 +0200

    User specific vars moved to .env

--- models/llama.py
+++ models/llama.py
@@ -3,10 +3,13 @@ from transformers import LlamaForCausalLM, LlamaTokenizer
 
 # https://huggingface.co/meta-llama/Llama-2-7b-chat-hf
 class LLaMA2_7B_ChatHF(LLM):
+    def __init__(self, huggingface_token: str, default_temperature: float = 0, auto_load: bool = False) -> None:
+        self.huggingface_token = huggingface_token
+        super().__init__(default_temperature, auto_load)
+        
     def _load(self) -> None:
-        self.model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", device_map="auto")
-        self.tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", padding_side="left")
+        self.model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", device_map="auto", token=self.huggingface_token)
+        self.tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", padding_side="left", token=self.huggingface_token)
 
     def extend_generation_config(self, generation_config: dict) -> None:
         pass
-        # generation_config["pad_token_id"] = self.tokenizer.eos_token_id
