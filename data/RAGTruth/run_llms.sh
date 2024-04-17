0d7f669234a44d526c3a0157eb3172ebfd2a287c
F4biian <fabian@fotofactory-ridder.de>
2024-04-17 14:31:30 +0200

    Clean up + script execution with args

---
+++ data/RAGTruth/run_llms.sh
@@ -0,0 +1,13 @@
+python3 data/RAGTruth/internal_states_from_data.py mistral-7B-instruct 0 5 ""
+python3 data/RAGTruth/internal_states_from_data.py mistral-7B-instruct 1 5 "float8"
+python3 data/RAGTruth/internal_states_from_data.py mistral-7B-instruct 2 5 "int8"
+python3 data/RAGTruth/internal_states_from_data.py mistral-7B-instruct 3 5 "int4"
+
+# python3 data/RAGTruth/internal_states_from_data.py llama-2-7b-chat 4 512 ""
+# python3 data/RAGTruth/internal_states_from_data.py llama-2-7b-chat 5 512 "float8"
+# python3 data/RAGTruth/internal_states_from_data.py llama-2-7b-chat 6 512 "int8"
+# python3 data/RAGTruth/internal_states_from_data.py llama-2-7b-chat 7 512 "int4"
+
+# python3 data/RAGTruth/internal_states_from_data.py llama-2-13b-chat 8 512 "float8"
+# python3 data/RAGTruth/internal_states_from_data.py llama-2-13b-chat 9 512 "int8"
+# python3 data/RAGTruth/internal_states_from_data.py llama-2-13b-chat 10 512 "int4"
