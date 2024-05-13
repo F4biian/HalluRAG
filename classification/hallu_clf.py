18963af72f1cd5cc4c43874f9368fb39188f389a
F4biian <fabian@fotofactory-ridder.de>
2024-05-13 21:58:52 +0200

    Final readjustment of MLP arch because of mistake

--- classification/hallu_clf.py
+++ classification/hallu_clf.py
@@ -11,17 +11,16 @@ class HallucinationClassifier(nn.Module):
     def __init__(self, input_size) -> None:
         super(HallucinationClassifier, self).__init__()
         self.fc1 = nn.Linear(input_size, 256)
-        self.fc4 = nn.Linear(256, 128)
-        self.fc5 = nn.Linear(128, 64)
-        self.fc5 = nn.Linear(64, 1)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
-        x = self.sigmoid(self.fc5(x))
+        x = self.sigmoid(self.fc4(x))
         return x
 
 def train_model(
