2d1d39bcd85e230bf2ba6d4d221f9bca39147242
F4biian <fabian@fotofactory-ridder.de>
2024-05-17 23:42:32 +0200

    Removed temp code

--- classification/baseline_clf_train.py
+++ classification/baseline_clf_train.py
@@ -219,8 +219,6 @@ def run(model_name, internal_states_name, runs=15):
 
         test_loss, test_acc, test_precision, test_recall, test_f1, test_fpr, test_tpr, test_roc_auc, test_P, test_R, test_auc_pr, test_conf_matrix = test_model(model, test_loader, criterion, None)
 
-        exit()
-
         run_results.append({
             "X_train.size": X_train.shape,
             "y_train.size": y_train.shape,
