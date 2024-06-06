ff402a60356dbcc99f36606dbf15a4f7bfd848c0
F4biian <fabian@fotofactory-ridder.de>
2024-06-06 16:59:59 +0200

    Ignoring gemma

--- data/qna2output/run_llms.sh
+++ data/qna2output/run_llms.sh
@@ -43,17 +43,17 @@ python3 data/qna2output/2output.py llama-2-13b-chat 750 "int8" test
 python3 data/qna2output/2output.py llama-2-13b-chat 750 "int4" test
 
 
-python3 data/qna2output/2output.py gemma-7b-it 750 "" train
-python3 data/qna2output/2output.py gemma-7b-it 750 "float8" train
-python3 data/qna2output/2output.py gemma-7b-it 750 "int8" train
-python3 data/qna2output/2output.py gemma-7b-it 750 "int4" train
+# python3 data/qna2output/2output.py gemma-7b-it 750 "" train
+# python3 data/qna2output/2output.py gemma-7b-it 750 "float8" train
+# python3 data/qna2output/2output.py gemma-7b-it 750 "int8" train
+# python3 data/qna2output/2output.py gemma-7b-it 750 "int4" train
 
-python3 data/qna2output/2output.py gemma-7b-it 750 "" val
-python3 data/qna2output/2output.py gemma-7b-it 750 "float8" val
-python3 data/qna2output/2output.py gemma-7b-it 750 "int8" val
-python3 data/qna2output/2output.py gemma-7b-it 750 "int4" val
+# python3 data/qna2output/2output.py gemma-7b-it 750 "" val
+# python3 data/qna2output/2output.py gemma-7b-it 750 "float8" val
+# python3 data/qna2output/2output.py gemma-7b-it 750 "int8" val
+# python3 data/qna2output/2output.py gemma-7b-it 750 "int4" val
 
-python3 data/qna2output/2output.py gemma-7b-it 750 "" test
-python3 data/qna2output/2output.py gemma-7b-it 750 "float8" test
-python3 data/qna2output/2output.py gemma-7b-it 750 "int8" test
-python3 data/qna2output/2output.py gemma-7b-it 750 "int4" test
+# python3 data/qna2output/2output.py gemma-7b-it 750 "" test
+# python3 data/qna2output/2output.py gemma-7b-it 750 "float8" test
+# python3 data/qna2output/2output.py gemma-7b-it 750 "int8" test
+# python3 data/qna2output/2output.py gemma-7b-it 750 "int4" test
