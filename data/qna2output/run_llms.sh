117f90978911cfcc6c2de933e30377fd3442863d
F4biian <fabian@fotofactory-ridder.de>
2024-06-06 17:02:01 +0200

    Adapted amount of responses to generate

--- data/qna2output/run_llms.sh
+++ data/qna2output/run_llms.sh
@@ -1,59 +1,59 @@
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "" train
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "float8" train
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "int8" train
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "int4" train
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "" train
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "float8" train
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "int8" train
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "int4" train
 
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "" val
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "float8" val
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "int8" val
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "int4" val
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "" val
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "float8" val
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "int8" val
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "int4" val
 
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "" test
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "float8" test
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "int8" test
-python3 data/qna2output/2output.py mistral-7B-instruct 750 "int4" test
-
-
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "" train
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "float8" train
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "int8" train
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "int4" train
-
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "" val
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "float8" val
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "int8" val
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "int4" val
-
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "" test
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "float8" test
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "int8" test
-python3 data/qna2output/2output.py llama-2-7b-chat 750 "int4" test
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "" test
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "float8" test
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "int8" test
+python3 data/qna2output/2output.py mistral-7B-instruct 1000 "int4" test
 
 
-python3 data/qna2output/2output.py llama-2-13b-chat 750 "float8" train
-python3 data/qna2output/2output.py llama-2-13b-chat 750 "int8" train
-python3 data/qna2output/2output.py llama-2-13b-chat 750 "int4" train
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "" train
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "float8" train
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "int8" train
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "int4" train
 
-python3 data/qna2output/2output.py llama-2-13b-chat 750 "float8" val
-python3 data/qna2output/2output.py llama-2-13b-chat 750 "int8" val
-python3 data/qna2output/2output.py llama-2-13b-chat 750 "int4" val
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "" val
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "float8" val
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "int8" val
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "int4" val
 
-python3 data/qna2output/2output.py llama-2-13b-chat 750 "float8" test
-python3 data/qna2output/2output.py llama-2-13b-chat 750 "int8" test
-python3 data/qna2output/2output.py llama-2-13b-chat 750 "int4" test
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "" test
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "float8" test
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "int8" test
+python3 data/qna2output/2output.py llama-2-7b-chat 1000 "int4" test
 
 
-# python3 data/qna2output/2output.py gemma-7b-it 750 "" train
-# python3 data/qna2output/2output.py gemma-7b-it 750 "float8" train
-# python3 data/qna2output/2output.py gemma-7b-it 750 "int8" train
-# python3 data/qna2output/2output.py gemma-7b-it 750 "int4" train
+python3 data/qna2output/2output.py llama-2-13b-chat 1000 "float8" train
+python3 data/qna2output/2output.py llama-2-13b-chat 1000 "int8" train
+python3 data/qna2output/2output.py llama-2-13b-chat 1000 "int4" train
 
-# python3 data/qna2output/2output.py gemma-7b-it 750 "" val
-# python3 data/qna2output/2output.py gemma-7b-it 750 "float8" val
-# python3 data/qna2output/2output.py gemma-7b-it 750 "int8" val
-# python3 data/qna2output/2output.py gemma-7b-it 750 "int4" val
+python3 data/qna2output/2output.py llama-2-13b-chat 1000 "float8" val
+python3 data/qna2output/2output.py llama-2-13b-chat 1000 "int8" val
+python3 data/qna2output/2output.py llama-2-13b-chat 1000 "int4" val
 
-# python3 data/qna2output/2output.py gemma-7b-it 750 "" test
-# python3 data/qna2output/2output.py gemma-7b-it 750 "float8" test
-# python3 data/qna2output/2output.py gemma-7b-it 750 "int8" test
-# python3 data/qna2output/2output.py gemma-7b-it 750 "int4" test
+python3 data/qna2output/2output.py llama-2-13b-chat 1000 "float8" test
+python3 data/qna2output/2output.py llama-2-13b-chat 1000 "int8" test
+python3 data/qna2output/2output.py llama-2-13b-chat 1000 "int4" test
+
+
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "" train
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "float8" train
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "int8" train
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "int4" train
+
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "" val
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "float8" val
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "int8" val
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "int4" val
+
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "" test
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "float8" test
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "int8" test
+# python3 data/qna2output/2output.py gemma-7b-it 1000 "int4" test
