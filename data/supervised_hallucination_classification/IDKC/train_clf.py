fcbae5efdd7e41f08ff035806fb569140c9fd83b
F4biian <fabian@fotofactory-ridder.de>
2024-06-12 11:24:27 +0200

    Finished training of IDKC

--- data/supervised_hallucination_classification/IDKC/train_clf.py
+++ data/supervised_hallucination_classification/IDKC/train_clf.py
@@ -3,6 +3,9 @@
 
 import fasttext
 import os
+import json
+import re
+import pandas as pd
 
 ########################################################################################
 
@@ -10,23 +13,90 @@ CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 TRAIN_FILE = os.path.join(CURR_DIR, "idk.train")
 TEST_FILE = os.path.join(CURR_DIR, "idk.test")
 MODEL_FILE = os.path.join(CURR_DIR, "idk_model.bin")
+BENCHMARK_DATA_FILE = os.path.join(CURR_DIR, "..", "manual_classification.json")
 TRAIN_PERC = 0.75
 CLEAN_REGEX = r'\d|!|"|#|\$|%|&|\'|\(|\)|\*|\+|,|-|\.|\/|:|;|<|=|>|\?|@|\[|\\|\]|\^|_|`|{|\||}|~'
 
-def create_files() -> None:
+def create_files(train_perc=0.75) -> None:
     # TODO: implement once data exist
-    # str = re.sub(CLEAN_REGEX, '', str.lower())
-    # str.replace('\s', ' ', regex=True) # replace every form of whitespace with a space
-    # remove stopwords wit nltk?
-    # add either __label__idk or __label__ik before each sentence (one space between label and sentence)
-    # split into train and test
-    # save files
-    raise NotImplementedError()
+
+    data = []
+    with open(BENCHMARK_DATA_FILE, "r") as file:
+        data = json.load(file)["data"]
+
+    all_sent_count = 0
+    for d in data:
+        all_sent_count += len(d["sentence_data"])
+
+    train_data = []
+
+    for d in data:
+        answerable = d["prompt"]["answerable"]
+
+        # print(d["model"])
+        # print(d["quantization"])
+
+        last_sent_end = 0
+        for sent_i, sent_data in enumerate(d["sentence_data"]):
+            sent = sent_data['cum_sentence'][last_sent_end:].strip()
+            human_label = sent_data["target"]
+            
+            if answerable:
+                if human_label == "W" or human_label == "IDK":
+                    target = 1
+                elif human_label == "R":
+                    target = 0
+                else:
+                    continue
+            else:
+                if human_label == "W" or human_label == "IDK":
+                    target = 1
+                elif human_label == "R":
+                    target = 0
+                else:
+                    continue
+
+            ft_sent = re.sub(CLEAN_REGEX, '', sent.lower())
+            ft_sent = re.sub(r'\s', ' ', ft_sent.strip())
+
+            ft_data = {
+                "target": target,
+                "label": human_label,
+                "ft_label": f"__label__{'idk' if human_label == 'IDK' else 'ik'}",
+                "sent": sent,
+                "ft_sent": ft_sent
+            }
+            train_data.append(ft_data)
+
+            last_sent_end = len(sent_data['cum_sentence'])
+        
+    train_df = pd.DataFrame(train_data)
+    test_df = train_df.iloc[int(train_df.shape[0]*train_perc):]
+    train_df = train_df.iloc[:int(train_df.shape[0]*train_perc)]
+
+    print(train_df)
+    print(train_df["ft_label"].value_counts())
+    print(test_df)
+    print(test_df["ft_label"].value_counts())
+
+    train_file_content = ""
+    for _, row in train_df.iterrows():
+        train_file_content += f"{row['ft_label']} {row['ft_sent']}\n"
+    train_file_content = train_file_content.strip()
+    with open(TRAIN_FILE, "w") as file:
+        file.write(train_file_content)
+
+    test_file_content = ""
+    for _, row in test_df.iterrows():
+        test_file_content += f"{row['ft_label']} {row['ft_sent']}\n"
+    test_file_content = test_file_content.strip()
+    with open(TEST_FILE, "w") as file:
+        file.write(test_file_content)
 
 create_files()
 
 # Train model
-model = fasttext.train_supervised(input=TRAIN_FILE) # , lr=1.0, epoch=25, wordNgrams=2, dim=50
+model = fasttext.train_supervised(input=TRAIN_FILE, lr=0.3, epoch=50, wordNgrams=2, dim=50) # , lr=1.0, epoch=25, wordNgrams=2, dim=50
 
 # Show train results
 train_results = model.test(TRAIN_FILE)
