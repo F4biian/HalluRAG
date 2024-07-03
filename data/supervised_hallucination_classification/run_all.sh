3cc4ecd85f5b21613f6ba832e17a4ba7c9c39f23
F4biian <fabian@fotofactory-ridder.de>
2024-07-03 17:58:22 +0200

    Fixed typos

--- data/supervised_hallucination_classification/run_all.sh
+++ data/supervised_hallucination_classification/run_all.sh
@@ -1,50 +1,46 @@
-python3 data/supervised_hallucination_classification/run_shd.py train_meta-llama_Llama-2-7b-chat-hf.pickle
-python3 data/supervised_hallucination_classification/run_shd.py val_meta-llama_Llama-2-7b-chat-hf.pickle
+# python3 data/supervised_hallucination_classification/run_shd.py train_meta-llama_Llama-2-7b-chat-hf.pickle
+# python3 data/supervised_hallucination_classification/run_shd.py val_meta-llama_Llama-2-7b-chat-hf.pickle
 
-# python3 data/supervised_hallucination_classification/run_shd.py train_meta-llama_Llama-2-7b-chat-hf\ (int8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py val_meta-llama_Llama-2-7b-chat-hf\ (int8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_meta-llama_Llama-2-7b-chat-hf\ (int8).pickle
+# python3 data/supervised_hallucination_classification/run_shd.py "train_meta-llama_Llama-2-7b-chat-hf (int8).pickle"
+# python3 data/supervised_hallucination_classification/run_shd.py "val_meta-llama_Llama-2-7b-chat-hf (int8).pickle"
+# python3 data/supervised_hallucination_classification/run_shd.py "test_meta-llama_Llama-2-7b-chat-hf (int8).pickle"
 
-# python3 data/supervised_hallucination_classification/run_shd.py train_meta-llama_Llama-2-7b-chat-hf\ (float8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py val_meta-llama_Llama-2-7b-chat-hf\ (float8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_meta-llama_Llama-2-7b-chat-hf\ (float8).pickle
+# python3 data/supervised_hallucination_classification/run_shd.py "train_meta-llama_Llama-2-7b-chat-hf (float8).pickle"
+# python3 data/supervised_hallucination_classification/run_shd.py "val_meta-llama_Llama-2-7b-chat-hf (float8).pickle"
+# python3 data/supervised_hallucination_classification/run_shd.py "test_meta-llama_Llama-2-7b-chat-hf (float8).pickle"
 
-# python3 data/supervised_hallucination_classification/run_shd.py train_meta-llama_Llama-2-7b-chat-hf\ (int4).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py val_meta-llama_Llama-2-7b-chat-hf\ (int4).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_meta-llama_Llama-2-7b-chat-hf\ (int4).pickle
-
-
-
-# python3 data/supervised_hallucination_classification/run_shd.py train_mistralai_Mistral-7B-Instruct-v0.1.pickle
-# python3 data/supervised_hallucination_classification/run_shd.py val_mistralai_Mistral-7B-Instruct-v0.1.pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_mistralai_Mistral-7B-Instruct-v0.1.pickle
-
-# python3 data/supervised_hallucination_classification/run_shd.py train_mistralai_Mistral-7B-Instruct-v0.1\ (int8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py valmistralai_Mistral-7B-Instruct-v0.1f\ (int8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_mistralai_Mistral-7B-Instruct-v0.1\ (int8).pickle
-
-# python3 data/supervised_hallucination_classification/run_shd.py train_mistralai_Mistral-7B-Instruct-v0.1\ (float8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py val_mistralai_Mistral-7B-Instruct-v0.1\ (float8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_mistralai_Mistral-7B-Instruct-v0.1\ (float8).pickle
-
-# python3 data/supervised_hallucination_classification/run_shd.py train_mistralai_Mistral-7B-Instruct-v0.1\ (int4).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py val_mistralai_Mistral-7B-Instruct-v0.1\ (int4).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_mistralai_Mistral-7B-Instruct-v0.1\ (int4).pickle
+# python3 data/supervised_hallucination_classification/run_shd.py "train_meta-llama_Llama-2-7b-chat-hf (int4).pickle"
+# python3 data/supervised_hallucination_classification/run_shd.py "val_meta-llama_Llama-2-7b-chat-hf (int4).pickle"
+# python3 data/supervised_hallucination_classification/run_shd.py "test_meta-llama_Llama-2-7b-chat-hf (int4).pickle"
 
 
 
-# python3 data/supervised_hallucination_classification/run_shd.py train_meta-llama_Llama-2-13b-chat-hf.pickle
-# python3 data/supervised_hallucination_classification/run_shd.py val_meta-llama_Llama-2-13b-chat-hf.pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_meta-llama_Llama-2-13b-chat-hf.pickle
+python3 data/supervised_hallucination_classification/run_shd.py "train_mistralai_Mistral-7B-Instruct-v0.1.pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "val_mistralai_Mistral-7B-Instruct-v0.1.pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "test_mistralai_Mistral-7B-Instruct-v0.1.pickle"
 
-# python3 data/supervised_hallucination_classification/run_shd.py train_meta-llama_Llama-2-13b-chat-hf\ (int8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py val_meta-llama_Llama-2-13b-chat-hf\ (int8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_meta-llama_Llama-2-13b-chat-hf\ (int8).pickle
+python3 data/supervised_hallucination_classification/run_shd.py "train_mistralai_Mistral-7B-Instruct-v0.1 (int8).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "val_mistralai_Mistral-7B-Instruct-v0.1f (int8).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "test_mistralai_Mistral-7B-Instruct-v0.1 (int8).pickle"
 
-# python3 data/supervised_hallucination_classification/run_shd.py train_meta-llama_Llama-2-13b-chat-hf\ (float8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py val_meta-llama_Llama-2-13b-chat-hf\ (float8).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_meta-llama_Llama-2-13b-chat-hf\ (float8).pickle
+python3 data/supervised_hallucination_classification/run_shd.py "train_mistralai_Mistral-7B-Instruct-v0.1 (float8).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "val_mistralai_Mistral-7B-Instruct-v0.1 (float8).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "test_mistralai_Mistral-7B-Instruct-v0.1 (float8).pickle"
 
-# python3 data/supervised_hallucination_classification/run_shd.py train_meta-llama_Llama-2-13b-chat-hf\ (int4).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py val_meta-llama_Llama-2137b-chat-hf\ (int4).pickle
-# python3 data/supervised_hallucination_classification/run_shd.py test_meta-llama_Llama-2-13b-chat-hf\ (int4).pickle
+python3 data/supervised_hallucination_classification/run_shd.py "train_mistralai_Mistral-7B-Instruct-v0.1 (int4).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "val_mistralai_Mistral-7B-Instruct-v0.1 (int4).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "test_mistralai_Mistral-7B-Instruct-v0.1 (int4).pickle"
+
+
+
+python3 data/supervised_hallucination_classification/run_shd.py "train_meta-llama_Llama-2-13b-chat-hf (int8).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "val_meta-llama_Llama-2-13b-chat-hf (int8).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "test_meta-llama_Llama-2-13b-chat-hf (int8).pickle"
+
+python3 data/supervised_hallucination_classification/run_shd.py "train_meta-llama_Llama-2-13b-chat-hf (float8).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "val_meta-llama_Llama-2-13b-chat-hf (float8).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "test_meta-llama_Llama-2-13b-chat-hf (float8).pickle"
+
+python3 data/supervised_hallucination_classification/run_shd.py "train_meta-llama_Llama-2-13b-chat-hf (int4).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "val_meta-llama_Llama-2-13b-chat-hf (int4).pickle"
+python3 data/supervised_hallucination_classification/run_shd.py "test_meta-llama_Llama-2-13b-chat-hf (int4).pickle"
